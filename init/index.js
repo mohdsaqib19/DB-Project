05214f80a34573c947abf9b3f5e2dd2505912c23
Mohd Saqib <nssaqib99@gmail.com>
2026-01-03 18:44:21 +0530

    initialize data

---
+++ init/index.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const initData = require('./data.js');
+const Listing = require('../models/listing.js');
+const MONGO_URL = "mongodb://localhost:27017/wanderlust";
+main()
+.then(() => console.log("Connected to MongoDB"))
+.catch((err) => console.log(err));
+
+
+async function main() {
+    await mongoose.connect(MONGO_URL);
+};
+const initDB = async () => {
+    await Listing.deleteMany({});
+    await Listing.insertMany(initData.data);
+    console.log("Database Initialized with Sample Data");
+};
+initDB();
